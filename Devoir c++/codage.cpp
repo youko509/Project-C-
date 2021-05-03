4ede7147f6c293fa984362d4481932565f691f87
Youko-coder <saintcharleserico@gmail.com>
2021-05-03 12:02:40 -0400

    add gestion etudiant

--- Devoir c++/codage.cpp
+++ Devoir c++/codage.cpp
@@ -8,9 +8,179 @@
 #include<math.h>
 #include<cstring>
 #include<vector>
+#include<fstream>
 using namespace std;
 
+struct Etudiant{
+string code;
+string nom;
+string prenom;
+string adresse;
+};
+bool is_unique(string b)
+{
+    ifstream back_code;
+    back_code.open("back_code.txt", ios::app);
+    string line;
+    while(getline(back_code,line))
+    {
+        if(line == b)
+        {
+            return false;
+        }
+        else
+        {
+            true;
+        }
+    }
+}
+void add_etudiant(){
+    struct Etudiant e;
+    int j,i,nbr;
+    string t;
+    ofstream file;
+    ofstream back_code;//un autre flux creer pour stocker les codes etudiants dans un fichier a part
+    file.open("backup.txt",ios::app);
+    back_code.open("back_code.txt", ios::app);
+    j=4;
+    bool test;
+    cout<<"Entrez le nombre d'etudiant"<<endl;
+    cin>>nbr;
+    string tab[nbr][j];
+    for( i=0;i<nbr;i++){
+        cout<<"Entrer l'etudiant "<<i+1<<endl;
+        for( j=0;j<4;j++){
+            fflush(stdin);
+            switch(j){
+                case 0:
+                    cout<<"Nom: "<<endl;
+                    getline(cin,e.nom);
+                    tab[i][j]=e.nom;
+                    break;
+                case 1:
+                    cout<<"Prenom: "<<endl;
+                    getline(cin,e.prenom);
+                    tab[i][j]=e.prenom;
+                    break;
+                case 2:
+                    test=true;
+                     while(test){
+                        fflush(stdin);
+                        cout<<"Code: "<<endl;
+                        getline(cin,e.code);
+                        if (is_unique(e.code))
+                        {
+                            back_code<<e.code;
+                            back_code<<endl;
+                            test=false;
+                        }else{
+                            cout<<"echec  le code doit etre unique"<<endl;
+                        }
+                    }
+                    tab[i][j]=e.code;
+                    break;
+                case 3:
+                    cout<<"Adresse "<<endl;
+                    getline(cin,e.adresse);
+                    tab[i][j]=e.adresse;
+                    break;
+            }
+        }
+         if(file && back_code){
+             file<<e.nom<<"\t"<<e.prenom<<"\t"<<e.code<<"\t"<<e.adresse;
+             file<<endl;
 
+
+
+    }else{
+        cout<<"le fichier n'est pas creer";
+    }
+}
+file.close();
+ back_code.close();
+    }
+void display_etudiant()
+{
+    int data = 0;
+    string line;
+    char print;
+    ifstream file;
+    file.open("backup.txt",ios::in);
+    while(getline(file,line))
+    {
+        data++;
+    }
+    file.close();
+    file.open("backup.txt",ios::in);
+    if(data>0)
+    {
+        while(getline(file,line))
+        {
+            cout<<line<<endl;
+        }
+        cout<<"taper p pour imprimer le fichier"<<endl;
+        cin>>print;
+        if( int (print)==112 || int(print)==80)
+        {
+            string k = ("backup.txt");
+            system(k.c_str());//commande system utilis� pour imprimer un fichier
+        }
+    }
+    else
+    {
+        cout<<"la base de donnee est vide, relancer le programme et inscrivez des etudiants d'abord"<<endl;
+    }
+}
+void data_class()
+{
+    string line;
+    int nb_line = 0;
+    char print;
+    ifstream file;
+    int i = 0;
+    file.open("backup.txt",ios::in);
+    while(getline(file,line))//determinon la quantite de ligne qu'il y a dans le fichier
+    {
+        nb_line++;
+    }
+    file.close();
+    if(nb_line>0)
+    {
+        file.open("backup.txt",ios::in);
+        string tab[nb_line];
+        while(getline(file,line))//stockons les valeur extrait du fichier dans un tableau
+        {
+            tab[i] = line;
+            //si la premiere caractere du nom est minuscule changer lla en majuscule
+            if(int (tab[i][0]) >97 && int (tab[i][0])<122)
+            {
+               tab[i][0]=char (int (tab[i][0])-32);
+            }
+            i++;
+        }
+        file.close();
+        //trie du tableau par ordre alphabetique
+        string temp;
+        for (int p = 0; p<nb_line;p++)
+        {
+            for(int i = 0; i<nb_line-p;i++)
+            {
+                if(tab[i] > tab[i+1])
+                {
+                    temp=tab[i];
+                    tab[i]=tab[i+1];
+                    tab[i+1]=temp;
+                }
+            }
+        }
+
+
+        for (int i = 0; i<nb_line; i++)//stockons les valeur extrait du fichier dans un tableau
+        {
+            cout<<tab[i]<<endl;
+        }
+    }
+}
 void Menu(){
     cout<<"------------------------------------------Bienvenue dans mon Programme------------------------------"<<endl;
     cout<<"Faites un choix parmi la liste ci-dessus: "<<endl;
@@ -309,6 +479,7 @@ void HextoDec(string trem, int test){
     int i,g,j,d,tem;
     string t[6]={"A","B","C","D","E","F"};
     string p[6]={"a","b","c","d","e","f"};
+    string all_ch[22]={"A","B","C","D","E","F","a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"};
     int h[6]={10,11,12,13,14,15};
     string s[10]={"0","1","2","3","4","5","6","7","8","9"};
     stringstream ss,ff,tt;
@@ -321,6 +492,8 @@ void HextoDec(string trem, int test){
     }
     j=sizeof(str)/sizeof(str[0]);
     g=j;
+
+
         for(int i=0;i<j;i++){
             for(int e=0;e<6;e++){
                 if(str[i]==t[e] || str[i]==p[e]){
@@ -355,8 +528,6 @@ void HextoDec(string trem, int test){
 
 
 }
-
-
 int main(){
     int number,test;
     string tem;
@@ -454,8 +625,9 @@ int main(){
                             }else if(choix3==2){
                                 cout<<"Entrez le nombre"<<endl;
                                 test=1;
-                                cin>>str;
-                                //BintoDecimal(str,test);
+                                cin>>number;
+                                EntierValid(number);
+                                BintoDecimal(number,test);
                                 cout<<"Voulez-vous continuez? tapez y pour une operation de decode, etc .... ou o pour retourner au menu "<<endl;
                                 cin>>temp;
                                 if(temp=="y"){
@@ -470,8 +642,9 @@ int main(){
                             }else if(choix3==3){
                                 cout<<"Entrez le nombre"<<endl;
                                 test=2;
-                                cin>>str;
-                                //BintoDecimal(str,test);;
+                                cin>>number;
+                                EntierValid(number);
+                                BintoDecimal(number,test);
                                 cout<<"Voulez-vous continuez? tapez y pour une operation de decode, etc .... ou o pour retourner au menu "<<endl;
                                 cin>>temp;
                                 if(temp=="y"){
@@ -536,6 +709,37 @@ int main(){
                                 break;
                             }
                         }
+                    }
+
+                }
+                }else if (choix1==2){
+
+                }else if(choix1==3){
+                    while(temp=="y"){
+                    cout<<"||1... Saisir les informations sur un etudiants             ||"<<endl;
+                    cout<<"||2... Affichage des informations du fichier                ||"<<endl;
+                    cout<<"||3... Afficher les informations par ordre alphabetiques    ||"<<endl;
+                    cin>>choix2;
+                    switch(choix2){
+                        case 1:
+                            add_etudiant();
+                            break;
+                        case 2:
+                            display_etudiant();
+                            break;
+                        case 3:
+                            data_class();
+                            break;
+                    }
+                    cout<<"Voulez-vous continuez? tapez y pour une operation de decode, etc .... ou o pour retourner au menu "<<endl;
+                    cin>>temp;
+                    if(temp=="y"){
+                        temp = "y";
+                    }else if(temp==temp2){
+                       temp2 ="o";
+                    }else{
+                        temp2="f";
+                        break;
                     }
 
                 }
